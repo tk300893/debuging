ac0eae6f9e9eef2dc0bb04fd544529cb669d2b0e
Tushar <Tushar@Tusshar_PC>
2019-04-29 15:46:07 +0530

    First commit

---
+++ debuging/src/lession1/App.java
@@ -0,0 +1,21 @@
+package lession1;
+
+public class App {
+
+	public static void main(String[] args)
+	{
+	for(int i=0;i<10;i++) 
+	{
+		for(int j=0;j<i;j++) {
+			System.out.print("*");
+		}
+	System.out.println();
+	
+	}
+	
+	
+		
+
+	}
+
+}
